4b489cc1ba6284ed820046a0b9e97a68d092a404
hama1080 <12245259+hama1080@users.noreply.github.com>
2019-08-21 14:25:52 +0900

    Update train.py

--- train.py
+++ train.py
@@ -79,8 +79,9 @@ def start_train():
         # Start running operations on the Graph.
         #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1.0)
         #session_config = tf.ConfigProto(gpu_options=gpu_options)
-        session_config = tf.ConfigProto()
-        session_config.gpu_options.allow_growth = True
+        gpu_options = tf.GPUOptions(allow_growth=True)
+        session_config = tf.ConfigProto(gpu_options=gpu_options)
+        #session_config.gpu_options.allow_growth = True
 
         nan_hook = tf.train.NanTensorHook(loss_tensor=loss)
         saver_hook = tf.train.CheckpointSaverHook(saver=saver, checkpoint_dir=FLAGS.parameter_dir, save_steps=1000)
